da7af4c7fdaa5c3ab69e16feda701c32a4d50f04
up230063 <up230063@alumnos.upa.edu.mx>
2026-03-01 21:25:54 -0600

    ejercicios terminados

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,52 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+evaluarNota : Float -> GradeStatus
+evaluarNota nota =
+    if nota < 0 then
+        Pending
+
+    else if nota >= 7 then
+        Approved
+
+    else
+        Failed
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map evaluarNota grades
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
